d1ecebb30fdcabb3f813e22dcf81d91c4ea3867f
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2024-03-13 13:42:29 +0900

    feat: main UI 완료

--- RoadBuddy/MainViewController.swift
+++ RoadBuddy/MainViewController.swift
@@ -18,11 +18,37 @@ class MainViewController: UIViewController {
     private var findingWayButton: UIButton = {
         let button = UIButton()
         button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 10
         button.widthAnchor.constraint(equalToConstant: 60).isActive = true
         button.heightAnchor.constraint(equalToConstant: 60).isActive = true
+        
+        
+        // 그림자 효과
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOffset = CGSize(width: 2.0, height: 2.0)
+        button.layer.shadowOpacity = 0.2
+        button.layer.shadowRadius = 2.0
+        
+        let image = UIImage(named: "direction_on")
+        button.setImage(image, for: .normal)
         return button
     }()
     
+    private var currentLocationButton: UIButton = {
+        let button = UIButton()
+        button.translatesAutoresizingMaskIntoConstraints = false
+        button.backgroundColor = .systemBlue
+        button.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        button.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        button.layer.cornerRadius = 40 / 2
+        button.clipsToBounds = true
+        
+        let image = UIImage(named: "direction_off")
+        button.setImage(image, for: .normal)
+        
+        return button
+    }()
     
     private var mapView: GMSMapView!
     
@@ -46,6 +72,7 @@ class MainViewController: UIViewController {
     private func configureUI() {
         self.view.addSubview(searchBarTextField)
         self.view.addSubview(findingWayButton)
+        self.view.addSubview(currentLocationButton)
         
         setConstraints()
     }
@@ -59,6 +86,9 @@ class MainViewController: UIViewController {
             
             findingWayButton.topAnchor.constraint(equalTo: safeArea.topAnchor, constant: 8),
             findingWayButton.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor, constant: -8),
+            
+            currentLocationButton.bottomAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -32),
+            currentLocationButton.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor, constant: 8),
         ])
     }
 }
