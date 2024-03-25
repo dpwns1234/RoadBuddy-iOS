79ed6cc0c7a069c1d84d4bb7866bb60486a57188
dpwns1234 <52391722+dpwns1234@users.noreply.github.com>
2024-03-25 16:00:02 +0900

    feat: 검색기록 저장하는 UserDefault repository 구현

---
+++ RoadBuddy/Repository/UserDefaultRepository.swift
@@ -0,0 +1,33 @@
+//
+//  UserDefaultRepository.swift
+//  RoadBuddy
+//
+//  Created by 김예준 on 3/25/24.
+//
+
+import UIKit
+
+final class UserDefaultRepository<T: Codable> {
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+    
+    func save(data: T) {
+        do {
+            let encoded = try encoder.encode(data)
+            UserDefaults.standard.setValue(encoded, forKey: "searchHistories")
+            UserDefaults.standard.synchronize()
+        } catch {
+            print("Failed encode: \(error)")
+        }
+    }
+    
+    func fetch() -> T? {
+        let data = UserDefaults.standard.object(forKey: "searchHistories") as! Data
+        do {
+            return try decoder.decode(T.self, from: data)
+        } catch {
+            print("Failed decode:\(error)")
+        }
+        return nil
+    }
+}
